38df3da00b809e2ad1b3e7fe8baf8e8cec7580c7
Hyun Don Moon <hyundonmoon@gmail.com>
2021-05-21 14:16:21 +0900

    [UPDATED] modified dropdown menu items, width, and added sample image

--- src/components/Header/Dropdown/Dropdown.tsx
+++ src/components/Header/Dropdown/Dropdown.tsx
@@ -8,8 +8,9 @@ const DropdownContainer = styled.div`
   position: absolute;
   top: 75px;
   height: 300px;
-  min-width: 120px;
+  min-width: 150px;
   border-radius: 4px;
+  background: rgba(255, 255, 255, 0.9);
   box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.25);
   right: 0;
 
@@ -25,12 +26,12 @@ const DropdownListContainer = styled.div`
   align-items: center;
 `;
 
-const Dropdown = () => {
+const Dropdown = ({ isLogin }: { isLogin: boolean }) => {
   return (
     <DropdownContainer>
       <DropdownListContainer>
-        <ShortDropDownMenu />
-        <LongDropdownMenu />
+        <ShortDropDownMenu isLogin={isLogin} />
+        <LongDropdownMenu isLogin={isLogin} />
       </DropdownListContainer>
     </DropdownContainer>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,12 +6,13 @@ import Logo from "./Logo";
 import NavBar from "./NavBar";
 
 const Header = styled.header`
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   max-width: 100%;
   width: 100vw;
   height: 80px;
+  background: rgba(255, 255, 255, 0.9);
 `;
 
 const HeaderWrapper = styled.div`
