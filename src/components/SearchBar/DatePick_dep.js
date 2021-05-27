54ff004798760c342117cfec86a0be1f7efbb491
김원호 <hodu_kim@Hodu-kimui-MacBP.local>
2021-05-27 16:26:05 +0900

    [CREATE] create-searchbar-component

---
+++ src/components/SearchBar/DatePick_dep.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import DatePicker, { registerLocale } from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import ko from "date-fns/locale/ko";
+import { Pick_Arr } from "./DatePickUtils";
+registerLocale("ko", ko);
+
+const DatePick_dep = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
+  return (
+    <>
+      <DatePicker
+        className="Picker"
+        selected={selectedDate}
+        onChange={(date) => setSelectedDate(date)}
+        dateFormat="yyyy/MM/dd(eee)"
+        minDate={new Date()}
+        placeholderText="날짜입력"
+        locale="ko"
+      />
+    </>
+  );
+};
+export default DatePick_dep;
+
+//함수 명이 import명과 같으면 에러가 생김
