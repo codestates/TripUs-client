36932c5eaa91d98b3eccc55a2106186a785c772b
김원호 <hodu_kim@Hodu-kimui-MacBP.local>
2021-06-07 00:14:43 +0900

    [UPDATE] change-color-loader

--- src/components/Common/Loader.js
+++ src/components/Common/Loader.js
@@ -12,8 +12,8 @@ const rotate = keyframes`
 `;
 
 const LoaderComponent = styled.div`
-  border: 1.6rem solid pink;
-  border-top: 1.6rem solid purple;
+  border: 1.6rem solid #0099ff;
+  border-top: 1.6rem solid #9ae59a;
   border-radius: 50%;
   height: ${(props) => (props.size ? `${props.size}rem` : "5rem")};
   width: ${(props) => (props.size ? `${props.size}rem` : "5rem")};
